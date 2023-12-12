297216acc02ff9a6f14b08f8bd81c10c9f39eae2
Solida <solida.var@gmail.com>
2023-12-12 22:52:32 +0100

    middleware authentication

---
+++ src/backend/middleware/middleware-authentication.ts
@@ -0,0 +1,19 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token: = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const userId: string | JwtPayload = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw "Invalid user ID";
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error("Invalid request!"),
+    });
+  }
+};
